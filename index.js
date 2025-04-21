3455677040c0015297eb8e7bb8f99f3b2989680b
Oraya Chantanawat <ochantanawat@gmail.com>
2025-04-21 15:33:58 +0700

    chore: add the solution

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.use(express.json());
 
 // Simulated data for API
 const books = [
-  { id: 1, title: "1984", author: "George Orwell", genre: "Dystopian" },
+  {
+    id: 1,
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+  },
   {
     id: 2,
     title: "To Kill a Mockingbird",
@@ -20,27 +25,56 @@ app.get("/books", (req, res, next) => {
   setTimeout(() => {
     const { genre } = req.query;
     //TODO: ADD CODE HERE ⬇️ to Filter books by genre.
-    const filteredBooks = books.filter((book) => book.genre.includes(genre));
-    //TODO: ADD CODE HERE ⬇️
+    if (genre) {
+      const filteredBooks = books.filter((book) => book.genre.includes(genre));
+      res.json(filteredBooks);
+    } else {
+      res.json(books);
+    }
   }, 1000); // Simulate a 1-second delay
 });
 
 // GET specific book by ID with async/await
+// SetTimeout is asynchronous, but the code inside is synchronous.
+// That's why there is no try.. catch...
+// Simulate asynchronus operation
 app.get("/books/:id", async (req, res, next) => {
-  const book = await new Promise((resolve, reject) => {
-    setTimeout(() => {
+  try {
+    const book = await new Promise((resolve, reject) => {
+      // setTimeout(() => {
       const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
       if (foundBook) {
         resolve(foundBook);
       } else {
         //TODO: ADD CODE to reject the promise
+        reject(new Error("Book not found"));
       }
-    }, 1000); // Simulate a 1-second delay
-  });
-  //TODO: ADD CODE HERE ⬇️
+      // }, 1000); // Simulate a 1-second delay
+    });
+    res.json(book);
+  } catch (err) {
+    err.status = 404;
+    next(err);
+  }
 });
 
+// Middleware to handle 404 errors
 //TODO: ADD CODE HERE ⬇️
+app.use((err, res, next) => {
+  const error = new Error("Not Found");
+  err.status = 404;
+  next(error);
+});
+
+// General error handling middleware
+//TODO: ADD CODE HERE ⬇️
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const response = {
+    message: err.message || "Internal Server Error",
+    status: status,
+  };
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
